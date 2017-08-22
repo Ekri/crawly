eecd87cb749d2fa5fbea1b98d9732491bef0619c
Bartosz Maciega <b.maciega@futuremind.com>
2017-08-22 14:12:07 +0200

    cache for retrieving , make db without duplicates

---
+++ web_crawler/RetrieverCache.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+
+class RetrieverCache(object):
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+        c = self.conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS sites
+            (url text unique)''')
+        self.conn.commit()
+        self.cursor = self.conn.cursor()
+
+    def set(self, url):
+        print "url", url
+
+        """
+        store the content for a given domain and relative url
+        """
+        self.cursor.execute("INSERT OR IGNORE INTO sites VALUES (?)",
+                            (url,))
+        self.conn.commit()
+
+    def get(self, urlQuery):
+        """
+        return the content for a given domain and relative url
+        """
+        self.cursor.execute("SELECT content FROM sites WHERE  url=?",
+                            urlQuery)
+        row = self.cursor.fetchone()
+        if row:
+            return row[0]
+
+    def get_urls(self, url):
+        """
+        return all the URLS within a domain
+        """
+        self.cursor.execute("SELECT url FROM sites WHERE url=?", (url,))
+        # could use fetchone and yield but I want to release
+        # my cursor after the call. I could have create a new cursor tho.
+        # ...Oh well
+        return [row[0] for row in self.cursor.fetchall()]
--- web_crawler/website_retriever.py
+++ web_crawler/website_retriever.py
@@ -1,6 +1,7 @@
 import httplib2
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
+from RetrieverCache import RetrieverCache
 
 
 class website_retriever(object):
@@ -21,15 +22,23 @@ class website_retriever(object):
             htttp = httplib2.Http()
             currentUrl = self.links[numberVisited]
             numberVisited = numberVisited + 1
-            status, response = htttp.request(currentUrl)
-            self.parseHtml(response)
+            try:
+                status, response = htttp.request(currentUrl)
+                self.parseHtml(response)
+            except httplib2.HttpLib2Error as err:
+                print err
 
     def parseHtml(self, response):
         for link in BeautifulSoup(response, "html.parser", parse_only=SoupStrainer('a')):
             if link.has_attr('href'):
-                self.links.append(link['href'])
-                print link['href']
+                linkRetrieved = link['href']
+                print "retrieving...", linkRetrieved
+                self.cache.set(linkRetrieved)
+                self.links.append(linkRetrieved)
 
 
-retriv = website_retriever()
-retriv.retrieve("http://www.interia.pl")
+
+
+
+retriv = website_retriever(RetrieverCache("retrieved.db"), maxPages=10)
+retriv.retrieve("http://www.google.com")
