6cda12ee098290514e0db77aeb69120b8a227702
Bartosz Maciega <b.maciega@futuremind.com>
2017-08-23 14:59:14 +0200

    searching for particular word in website and then filter this website

--- web_crawler/website_retriever.py
+++ web_crawler/website_retriever.py
@@ -1,15 +1,17 @@
 import httplib2
+import urllib2
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
 from RetrieverCache import RetrieverCache
 
 
 class website_retriever(object):
-    def __init__(self, cache=None, maxPages=10):
+    def __init__(self, cache=None, wordCache=None, maxPages=10):
         self.cache = cache
         self.maxPages = maxPages
+        self.wordCache = wordCache
 
-    def retrieve(self, url):
+    def retrieve(self, url, phrase=None):
         self.links = []
         self.links.append(url)
         numberVisited = 0
@@ -25,6 +27,7 @@ class website_retriever(object):
             try:
                 status, response = htttp.request(currentUrl)
                 self.parseHtml(response)
+                self.seekWord(currentUrl, response, phrase)
             except httplib2.HttpLib2Error as err:
                 print err
 
@@ -36,9 +39,14 @@ class website_retriever(object):
                 self.cache.set(linkRetrieved)
                 self.links.append(linkRetrieved)
 
+    def seekWord(self, url, htlm, word):
+        # soup = BeautifulSoup(htlm, "html.parser")
+        # result = soup.findAll(text=word)
+        # print "result: ", result
+        website = urllib2.urlopen(url).read()
+        if word in website:
+            self.wordCache.set(url)
 
 
-
-
-retriv = website_retriever(RetrieverCache("retrieved.db"), maxPages=10)
-retriv.retrieve("http://www.google.com")
+retriv = website_retriever(RetrieverCache("retrieved.db"), RetrieverCache("word.db"), maxPages=1)
+retriv.retrieve("https://www.reuters.com/article/us-usa-afghanistan-minerals-idUSKCN1B102L", "dupa")
