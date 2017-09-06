b939dcb9e15ce57537659c355c3ebfa09026bb06
Bartosz Maciega <b.maciega@futuremind.com>
2017-09-06 11:07:21 +0200

    move words crawling logic to another class , cleaning in retriever class

--- web_crawler/website_retriever.py
+++ web_crawler/website_retriever.py
@@ -1,56 +1,52 @@
 import httplib2
-import urllib2
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
+from crawlies.Crawly import WordsCrawly
 from urls_cache import UrlCache
+import db_uris
 
 
 class website_retriever(object):
-    def __init__(self, cache=None, word_cache=None, max_pages=10):
+    def __init__(self, cache=None, max_pages=10):
         self.cache = cache
         self.maxPages = max_pages
-        self.wordCache = word_cache
         self.consumers = []
+        self.links = []
 
-    def addConsumers(self, *args):
+    def add_crawlies(self, *args):
         for consumer in args:
             self.consumers.append(consumer)
 
-    def retrieve(self, url, phrase=None):
-        self.links = []
+    def retrieve(self, url):
         self.links.append(url)
         number_visited = 0
 
         while number_visited < self.maxPages:
             http = httplib2.Http()
             current_url = self.links[number_visited]
-            number_visited = number_visited + 1
+
             try:
+                print "Fetching: " + current_url
                 status, response = http.request(current_url)
                 self.parse_html(response)
-                self.seek_word(current_url, phrase)
+                for crawly in self.consumers:
+                    crawly.crawl(current_url)
+
             except httplib2.HttpLib2Error as err:
-                print err
+                print "Fetching error: ", err
+
+            number_visited = number_visited + 1
 
     def parse_html(self, response):
         for link in BeautifulSoup(response, "html.parser", parse_only=SoupStrainer('a')):
             if link.has_attr('href'):
-                linkRetrieved = link['href']
-                print "retrieving...", linkRetrieved
-                self.cache.set(linkRetrieved)
-                self.links.append(linkRetrieved)
-
-    def seek_word(self, url, word):
-        try:
-            website = urllib2.urlopen(url).read()
-            if word in website:
-                print "Word is in website: ", url
-                self.wordCache.set(url)
-
-        except Exception as exc:
-            print exc
+                link_retrieved = link['href']
+                print "retrieving...", link_retrieved
+                self.cache.set(link_retrieved)
+                self.links.append(link_retrieved)
 
 
-retriv = website_retriever(UrlCache("db/retrieved.db"), UrlCache("db/matches.db"),
+retriv = website_retriever(UrlCache(db_uris.READ_URI),
                            max_pages=100)
-retriv.retrieve("http://biblia.deon.pl/", "wiara")
+retriv.add_crawlies(WordsCrawly(UrlCache("db/matches.db"), "wiara"))
+retriv.retrieve("http://biblia.deon.pl/")
